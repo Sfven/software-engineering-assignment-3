a23f4e08a403de9be558f62bed5d25fd6d92f909
dat82 <dat.dat82@gmail.com>
2023-09-20 16:47:07 -0500

    Initial commit

---
+++ assignment3.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+<<comment
+printf "Enter your numbers below. Enter '~' to finish entering numbers.\n"
+
+while read inputs
+do
+  [ "$inputs" == "~" ] && break
+  array=("${array[@]}" $inputs)
+done
+comment
+
+array=( "$@" )
+
+add() {
+
+  local internal_array=("$@")
+  local sum=0
+  for i in "${internal_array[@]}" ; do
+    (( sum += i ))
+  done
+  echo $sum
+
+}
+
+multiply() {
+
+  local internal_array=("$@")
+  local product=1
+  for i in ${internal_array[@]} ; do
+    (( product *= i ))
+  done
+  echo $product
+
+}
+
+reverse() {
+
+  local array=("$@")
+  local reversed_array=()
+  local length="${#array[@]}"
+  for ((i = length - 1; i >= 0; i--)); do
+      reversed_array+=("${array[i]}")
+  done
+  echo "${reversed_array[@]}"
+
+}
+
+sum=$(add "${array[@]}")
+echo "Total Sum: $sum"
+
+<<comment
+product=$(multiply "${array[@]}")
+echo "Total Product: $product"
+reversed_array=($(reverse "${array[@]}"))
+echo "Reversed array: ${reversed_array[@]}"
+reversed_sum=$(add "${reversed_array[@]}")
+echo "Total Reversed Sum: $reversed_sum"
+reversed_product=$(multiply "${reversed_array[@]}")
+echo "Total Reversed Product: $reversed_product"
+comment
